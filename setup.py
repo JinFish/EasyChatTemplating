0ab7857fe23ad2595a120e7d36ab64eba7dbdc90
huang <none>
2024-07-15 19:35:33 +0800

    version 0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="EasyChatTemplating",
-      version="0.1",
-      packages=find_packages(),
-      install_requires=[]
-      )
+setup(
+    name="EasyChatTemplating",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["transformers"],
+)
